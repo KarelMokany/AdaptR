1979de60cedfa2b6d61989b7b6abb82e6f75b09a
Karel Mokany <Karel.Mokany@csiro.au>
2016-04-28 14:56:08 +0200

    First basic package

--- R/AdaptR_run.R
+++ R/AdaptR_run.R
@@ -7,17 +7,19 @@
 ################################################################################
 
 # Set the working directory
-setwd("C:/Users/mok010/Karels Files/Mokany Files/My R applications/AdaptR/source")
+#setwd("C:/Users/mok010/Karels Files/Mokany Files/My R applications/AdaptR/source")
 
-# load the dll
-dyn.load("main.dll")
+
 
 ## Set the parameter file name
-Parameter.File.Path = "C:/Users/mok010/Karels Files/Mokany Files/My R applications/AdaptR/Test_Inputs/Parameters/Adaptor_parameters_ironensis_TEST.txt"
+#Parameter.File.Path = "C:/Users/mok010/Karels Files/Mokany Files/My R applications/AdaptR/Test_Inputs/Parameters/Adaptor_parameters_ironensis_TEST.txt"
 
 ## Run AdaptR
 AdaptR<-function(Pfile = Parameter.File.Path)
 {
+  # load the dll
+  dyn.load("src/main.dll")
+  # call AdaptR dll
   .C("AdaptR",  argv = as.character(c(Pfile)),
      arg_i_catch = as.integer(c(0,0)))
 } # end AdaptR function
