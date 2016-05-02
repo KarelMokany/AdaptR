65f57694b40a9c8631c0dede938d1f17dbf7d37f
Karel Mokany <Karel.Mokany@csiro.au>
2016-05-02 22:38:56 +0200

    Compactor Functions now active with short documentation

--- R/CompactGrids_run.R
+++ R/CompactGrids_run.R
@@ -2,7 +2,7 @@
 #' Create compacted environmental grids for AdaptR
 #'
 #' @param Pfile A file path to the parameter file
-#' @return A set of cpmacted environment files in the specified location
+#' @return A set of compacted environment files in the specified location
 #' @examples
 #' CompactGrids("C:/MyDocuments/CompactorParameterFile.txt")
 CompactGrids<-function(Pfile = Parameter.File.Path)
